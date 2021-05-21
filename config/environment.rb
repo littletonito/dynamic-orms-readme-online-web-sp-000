8fa9576805a82a3a1f485cfd3e9c59e6f6744c6e
Antonio Clavijo <littletonito@gmail.com>
2021-05-20 21:16:36 -0400

    commit tree 263a37c0ec6622a9e362c038c122fd905c484ef8

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,12 @@
 require 'sqlite3'
 
-
+# Create the database
 DB = {:conn => SQLite3::Database.new("db/songs.db")}
+
+# Drop table to avoid errors
 DB[:conn].execute("DROP TABLE IF EXISTS songs")
 
+# Create the table
 sql = <<-SQL
   CREATE TABLE IF NOT EXISTS songs (
   id INTEGER PRIMARY KEY,
@@ -12,5 +15,8 @@ sql = <<-SQL
   )
 SQL
 
+# Use results_as_hash to return the database as a hash and not as an array
+# DB[:conn].execute("SELECT * FROM songs LIMIT 1") will return [[1,"Hello",25]]
+# We want something like {"id"=>1, "name"=>"Hello", "album"=>"25", 0 => 1, 1 => "Hello", 2 => "25"}
 DB[:conn].execute(sql)
 DB[:conn].results_as_hash = true
